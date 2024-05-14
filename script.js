89bf57dab85631935390185dc4e0bbc5671286e9
Iury de Amorim Demberg <iuryamorimdev@gmail.com>
2024-05-14 18:04:32 -0400

    ...

---
+++ script.js
@@ -0,0 +1,29 @@
+console.log("oi, mundo");
+var nome = "Iury";
+console.log(nome);
+var idade = "25";
+console.log(idade);
+
+var comida = "";
+comida = "pizza";
+console.log(comida);
+
+var arroz = "";
+console.log(arroz);
+
+var feijao = "";
+console.log(feijao);
+
+var aprender = "";
+console.log(aprender);
+
+var amor = "";
+console.log(amor);
+
+var genio = "";
+console.log(genio);
+var nome = "Iury";
+console.log(nome);
+
+var idade = "25";
+console.log(idade);
